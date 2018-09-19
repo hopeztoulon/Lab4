94c62c48fb80b97549c795b2f3a76b5f58874c2c
Hope Toulon <hopeztoulon@gmail.com>
2018-09-19 12:36:25 -0400

    Start of Lab4 method

---
+++ Lab4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Lab4 {//start class
+	
+	public static void main(String[] args) {//start main
+		
+		Integer[] test1  = null;
+		Integer[] array4 = new Integer[] {};
+		Integer[] array1 = new Integer[] {15, 3, 8, 25};
+		Integer[] array2 = new Integer[] {12, 45, -4, 0};
+		Integer[] array3 = new Integer[] {2, 15, 21, 56};
+		
+	
+	}//end main
+
+	public Integer[] sortArray(Integer[] u)
+	{//start sort
+		for(int i = 0; i < u.length; i++) {//start for
+			
+		}//end for
+		
+		return u;
+	}//end sort
+	 
+}//end class
